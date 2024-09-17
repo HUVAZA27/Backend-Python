d8a0bbe6043c27a2178524630477e717d80e54b8
Huvaza <huvaza27gmail.com>
2024-09-17 14:48:29 -0600

    first commit

---
+++ Backend/type_hints.py
@@ -0,0 +1,8 @@
+my_string_variable = "My String variable"
+print(my_string_variable)
+print(type(my_string_variable))
+
+my_string_variable = 5
+print(my_string_variable)
+
+print(type(my_string_variable))
